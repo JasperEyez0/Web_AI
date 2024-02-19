cf0fc1cbfffa501042d0b9aa9d31615320eb340e
JasperEyez <119019473+JasperEyez@users.noreply.github.com>
2024-02-19 23:49:20 +0700

    update1

--- client/src/page/StudentAdd.js
+++ client/src/page/StudentAdd.js
@@ -6,6 +6,10 @@ import { IoIosSearch } from "react-icons/io";
 import { TiUserAdd } from "react-icons/ti";
 import { FaUserEdit } from "react-icons/fa";
 import { RiDeleteBin5Fill } from "react-icons/ri";
+import '../page/StudentAdd.css'; 
+
+import { Upload, message } from 'antd';
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
 
 import Axios from 'axios'
 import { useState, useEffect } from 'react'
@@ -35,17 +39,97 @@ const StudentAdd = () => {
         });
       };
         //6 = 24px 24 = 96px
+
+        const beforeUpload = (file) => {
+          const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+          if (!isJpgOrPng) {
+            message.error('You can only upload JPG/PNG file!');
+          }
+          const isLt2M = file.size / 1024 / 1024 < 2;
+          if (!isLt2M) {
+            message.error('Image must smaller than 2MB!');
+          }
+          return isJpgOrPng && isLt2M;
+        };
+          const [loading, setLoading] = useState(false);
+          const [imageUrl, setImageUrl] = useState();
+          const handleChange = (info) => {
+          if (info.file.status === 'uploading') {
+            setLoading(true);
+              return;
+            }
+         if (info.file.status === 'done') {
+        // Get this url from response in real world.
+        getBase64(info.file.originFileObj, (url) => {
+          setLoading(false);
+          setImageUrl(url);
+        });
+      }
+    };
+    const uploadButton = (
+      <button
+        style={{
+          border: 0,
+          background: 'none',
+        }}
+        type="button"
+      >
+        {loading ? <LoadingOutlined /> : <PlusOutlined />}
+        <div
+          style={{
+            marginTop: 8,
+          }}
+        >
+          Upload
+        </div>
+      </button>
+    );
+    const getBase64 = (img, callback) => {
+      const reader = new FileReader();
+      reader.addEventListener('load', () => callback(reader.result));
+      reader.readAsDataURL(img);
+    };
+
     return (
         <div className="flex flex-col w-auto h-screen bg-[#E1F7FF]">
-          <div className="flex flex-row w-full h-fit my-10 px-24 justify-start">
-            <form action="">
-              <div className="flex flex-row w-full">
-                  <input type="text" placeholder="Serach" className='w-[450px] px-2.5 py-[2px] rounded-l-lg'/>
-                  <button className='px-1 bg-sky-800 text-[#fff] rounded-r-lg text-[20px]'><IoIosSearch /></button>
+          <div className="flex flex-col w-full h-fit my-10 px-24 justify-start">
+            <div className="flex flex-row w-full justify-around">
+                <p className='min-w-[150px] px-2.5 py-[2px]'>ชื่อ</p>
+                <p className='min-w-[150px] px-2.5 py-[2px]'>สกุล</p>
+                <p className='min-w-[150px] px-2.5 py-[2px]'>รหัสนศ.</p>
+                <p className='min-w-[150px] px-2.5 py-[2px]'>วันเกิด</p>
+                <p className='min-w-[150px] px-2.5 py-[2px]'>เพศ</p>
+            </div>
+            <form action="" className='felx flex-col m-6 items-center justify-center '>
+              <div className="flex flex-row w-full justify-around">
+                  <input type="text" placeholder="ชื่อ" className='min-w-[100px] max-w-[150px] px-2.5 py-[2px] rounded-lg'/>
+                  <input type="text" placeholder="สกุล" className='min-w-[100px] max-w-[150px] px-2.5 py-[2px] rounded-lg'/>
+                  <input type="text" placeholder="รหัสนศ." className='min-w-[100px] max-w-[150px] px-2.5 py-[2px] rounded-lg'/>
+                  <input type="text" placeholder="วันเกิด" className='min-w-[100px] max-w-[150px] px-2.5 py-[2px] rounded-lg'/>
+                  <input type="text" placeholder="เพศ" className='min-w-[100px] max-w-[150px] px-2.5 py-[2px] rounded-lg'/>
+              </div>
+              <div className="text-center justify-center pt-10">
+                <Upload 
+                  name="avatar"
+                  listType="picture-card"
+                  className="avatar-uploader"
+                  showUploadList={false}
+                  action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
+                  beforeUpload={beforeUpload}
+                  onChange={handleChange}
+                  id='Upload-box'
+                >
+                  {imageUrl ? (
+                    <img
+                      src={imageUrl}
+                      alt="avatar"
+                      style={{width: '100%',}}/>)
+                    : (uploadButton)}
+                </Upload>
               </div>
             </form>
-            <div className="flex ml-6 items-center">
-              <NavLink  to={""} className="p-1 bg-sky-800 text-[#fff] rounded text-[20px]"><TiUserAdd /></NavLink>
+            <div className="flex m-6 items-center justify-center">
+              <button className='px-5 bg-sky-800 text-[#fff] rounded-lg text-[20px]'>Add</button>
             </div>
           </div>
         </div>
--- client/src/page/StudentEdit.js
+++ client/src/page/StudentEdit.js
@@ -4,6 +4,9 @@ import {useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom'
 import '../page/StudentEdit.css'; 
 
+import { Upload, message } from 'antd';
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
+
 import Axios from 'axios'
 import { useState, useEffect } from 'react'
 
@@ -32,6 +35,56 @@ const StudentEdit = () => {
         });
       };
         //6 = 24px 24 = 96px
+
+        const beforeUpload = (file) => {
+          const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+          if (!isJpgOrPng) {
+            message.error('You can only upload JPG/PNG file!');
+          }
+          const isLt2M = file.size / 1024 / 1024 < 2;
+          if (!isLt2M) {
+            message.error('Image must smaller than 2MB!');
+          }
+          return isJpgOrPng && isLt2M;
+        };
+          const [loading, setLoading] = useState(false);
+          const [imageUrl, setImageUrl] = useState();
+          const handleChange = (info) => {
+          if (info.file.status === 'uploading') {
+            setLoading(true);
+              return;
+            }
+         if (info.file.status === 'done') {
+        // Get this url from response in real world.
+        getBase64(info.file.originFileObj, (url) => {
+          setLoading(false);
+          setImageUrl(url);
+        });
+      }
+    };
+    const uploadButton = (
+      <button
+        style={{
+          border: 0,
+          background: 'none',
+        }}
+        type="button"
+      >
+        {loading ? <LoadingOutlined /> : <PlusOutlined />}
+        <div
+          style={{
+            marginTop: 8,
+          }}
+        >
+          Upload
+        </div>
+      </button>
+    );
+    const getBase64 = (img, callback) => {
+      const reader = new FileReader();
+      reader.addEventListener('load', () => callback(reader.result));
+      reader.readAsDataURL(img);
+    };
     return (
         <div className="flex flex-col w-auto h-screen bg-[#E1F7FF]">
           <div className="flex flex-col w-full h-fit my-10 px-24 justify-start">
@@ -50,11 +103,24 @@ const StudentEdit = () => {
                   <input type="date" placeholder="วันเกิด" className='min-w-[100px] max-w-[150px] px-2.5 py-[2px] rounded-lg'/>
                   <input type="text" placeholder="เพศ" className='min-w-[100px] max-w-[150px] px-2.5 py-[2px] rounded-lg'/>
               </div>
-              <div className="flex my-12 justify-center">
-              <label for="images" className="relative flex gap-2.5 flex-col justify-center items-center h-48 w-1/4 p-5 border-2 border-dashed border-[#555] rounded-lg text-[#444] cursor-pointer transition-all duration-200 ease-in-out bg-transparent hover:bg-[#D0E6EE]">
-                <span class="drop-title">Drop files here</span> or
-                <input type="file" accept="image/*" required/>
-              </label>
+              <div className="text-center justify-center pt-10">
+                <Upload 
+                  name="avatar"
+                  listType="picture-card"
+                  className="avatar-uploader"
+                  showUploadList={false}
+                  action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
+                  beforeUpload={beforeUpload}
+                  onChange={handleChange}
+                  id='Upload-box'
+                >
+                  {imageUrl ? (
+                    <img
+                      src={imageUrl}
+                      alt="avatar"
+                      style={{width: '100%',}}/>)
+                    : (uploadButton)}
+                </Upload>
               </div>
             </form>
             <div className="flex m-6 items-center justify-center">
