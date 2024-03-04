8c227df1aa56825a203393e7e26c1893f3b68053
JasperEyez <119019473+JasperEyez@users.noreply.github.com>
2024-03-04 21:59:55 +0700

    Update Report.js

--- client/src/page/Report.js
+++ client/src/page/Report.js
@@ -7,93 +7,9 @@ const Report = () => {
   //const token = localStorage.getItem('token');
   //console.log(token)
 
-  const [src, setSrc] = useState(null);
-  const [crop, setCrop] = useState({ aspect: 16 / 9 });
-  const [image, setImage] = useState(null);
-  const [output, setOutput] = useState(null);
-
-  useEffect(() => {
-    if (src) {
-      const img = new Image();
-      img.src = src;
-      img.onload = () => {
-        setImage(img);
-      };
-    }
-  }, [src]);
-
-  const selectImage = (file) => {
-    setSrc(URL.createObjectURL(file));
-  };
-
-  const onImageLoaded = useCallback((img) => {
-    setImage(img);
-    setCrop({ aspect: 16 / 9, width: 100, height: 100 });
-  }, []);
-
-  const cropImageNow = () => {
-    const canvas = document.createElement('canvas');
-    const scaleX = image.naturalWidth / image.width;
-    const scaleY = image.naturalHeight / image.height;
-    canvas.width = crop.width;
-    canvas.height = crop.height;
-    const ctx = canvas.getContext('2d');
-
-    const pixelRatio = window.devicePixelRatio;
-    canvas.width = crop.width * pixelRatio;
-    canvas.height = crop.height * pixelRatio;
-    ctx.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0);
-    ctx.imageSmoothingQuality = 'high';
-
-    ctx.drawImage(
-    image,
-    crop.x * scaleX,
-    crop.y * scaleY,
-    crop.width * scaleX,
-    crop.height * scaleY,
-    0,
-    0,
-    crop.width,
-    crop.height,
-    );
-    
-    // Converting to base64
-    const base64Image = canvas.toDataURL('image/jpeg');
-    setOutput(base64Image);
-};
-
-  console.log("src:", src);
-  console.log("image:", image);
-  console.log("crop:", crop);
-
   return (
     <div className="flex w-auto h-screen">
-      <center>
-        <input
-          type="file"
-          accept="image/*"
-          onChange={(e) => {
-            selectImage(e.target.files[0]);
-          }}
-        />
-        <br />
-        <br />
-        {src && (
-          <div>
-            <ReactCrop
-              src={src}
-              onImageLoaded={onImageLoaded}
-              crop={crop}
-              onChange={setCrop}
-            />
-            <br />
-            <button onClick={cropImageNow}>Crop</button>
-            <br />
-            <br />
-          </div>
-        )}
-        <div>{output && <img src={output} alt="" />}</div>
-      </center>
+      
     </div>
   );
 };
