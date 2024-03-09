c3e55f40d4c7a4ccccf3eba32fe836690bcec63a
JasperEyez <119019473+JasperEyez@users.noreply.github.com>
2024-03-09 22:49:44 +0700

    update reportImg
    กำหนดขนาด crop

--- client/src/page/ReportImgS.js
+++ client/src/page/ReportImgS.js
@@ -19,7 +19,10 @@ const ReportImgS = () => {
 
   const [file, setFile] = useState(null);
   const [src, setSrc] = useState(null);
-  const [crop, setCrop] = useState();
+  const [crop, setCrop] = useState({
+    width: 200,
+    height: 200,
+  });
   const [image, setImage] = useState(null);
   const [output, setOutput] = useState(null);
 
